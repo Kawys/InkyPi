b5dd38ff76b10e1337702fac2163a1e2759b9c3d
Kawys <70590739+Kawys@users.noreply.github.com>
2026-08-09 13:25:39 +0200

    delete hardware displays support

--- src/display/display_manager.py
+++ src/display/display_manager.py
@@ -1,4 +1,3 @@
-import fnmatch
 import json
 import logging
 
@@ -7,17 +6,6 @@ from display.mock_display import MockDisplay
 
 logger = logging.getLogger(__name__)
 
-# Try to import hardware displays, but don't fail if they're not available
-try:
-    from display.inky_display import InkyDisplay
-except ImportError:
-    logger.info("Inky display not available, hardware support disabled")
-
-try:
-    from display.waveshare_display import WaveshareDisplay
-except ImportError:
-    logger.info("Waveshare display not available, hardware support disabled")
-
 class DisplayManager:
 
     """Manages the display and rendering of images."""
@@ -25,7 +13,7 @@ class DisplayManager:
     def __init__(self, device_config):
 
         """
-        Initializes the display manager and selects the correct display type 
+        Initializes the display manager and selects the correct display type
         based on the configuration.
 
         Args:
@@ -34,28 +22,18 @@ class DisplayManager:
         Raises:
             ValueError: If an unsupported display type is specified.
         """
-        
+
         self.device_config = device_config
-     
+
         display_type = device_config.get_config("display_type", default="inky")
 
         if display_type == "mock":
             self.display = MockDisplay(device_config)
-        elif display_type == "inky":
-            self.display = InkyDisplay(device_config)
-        elif fnmatch.fnmatch(display_type, "epd*in*"):  
-            # derived from waveshare epd - we assume here that will be consistent
-            # otherwise we will have to enshring the manufacturer in the 
-            # display_type and then have a display_model parameter.  Will leave
-            # that for future use if the need arises.
-            #
-            # see https://github.com/waveshareteam/e-Paper
-            self.display = WaveshareDisplay(device_config)
         else:
             raise ValueError(f"Unsupported display type: {display_type}")
 
     def display_image(self, image, image_settings=[]):
-        
+
         """
         Delegates image rendering to the appropriate display instance.
 
@@ -69,7 +47,7 @@ class DisplayManager:
 
         if not hasattr(self, "display"):
             raise ValueError("No valid display instance initialized.")
-        
+
         # Save the image
         logger.info(f"Saving image to {self.device_config.current_image_file}")
         image.save(self.device_config.current_image_file)
